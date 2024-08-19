41537e2f145f50071e98e14ae37b7e689d536ec1
Arpita Sharma <arpita.arusharma@gmail.com>
2024-08-19 18:10:55 +0530

    start todo app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./App.css"
+import TodoInput from './components/TodoInput';
+
+function App() {
+  return (
+  <div className='main-container'>
+    <div className='centre-container'>
+       < TodoInput />
+    </div>
+  </div>
+  )
+}
+
+export default App
